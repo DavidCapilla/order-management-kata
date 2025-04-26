028ab5476dfff4fc2eef163bc3cfcf31c28e51cd
David Capilla Guilarte <david.capilla.guilarte@ast.cam.ac.uk>
2025-04-26 17:22:26 +0100

    dcg | Repackage

--- src/main/java/io/github/davidcapilla/order_management_kata/payment/PaymentDetails.java
+++ src/main/java/io/github/davidcapilla/order_management_kata/payment/PaymentDetails.java
@@ -1,7 +1,6 @@
 package io.github.davidcapilla.order_management_kata.payment;
 
 import io.github.davidcapilla.order_management_kata.payment.gateway.PaymentGateway;
-import io.github.davidcapilla.order_management_kata.payment.gateway.PaymentStatus;
 import io.github.davidcapilla.order_management_kata.product.Price;
 import java.time.LocalDateTime;
 
---
+++ src/main/java/io/github/davidcapilla/order_management_kata/payment/PaymentStatus.java
@@ -0,0 +1,5 @@
+package io.github.davidcapilla.order_management_kata.payment;
+
+public enum PaymentStatus {
+
+}
