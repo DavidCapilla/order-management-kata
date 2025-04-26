fb4ae9b56288f217b48fb07d1a595f79e6076f6f
David Capilla Guilarte <david.capilla.guilarte@ast.cam.ac.uk>
2025-04-26 16:12:15 +0100

    dcg | Add spring initializr structure

---
+++ src/main/java/io/github/davidcapilla/order_management_kata/OrderManagementKataApplication.java
@@ -0,0 +1,13 @@
+package io.github.davidcapilla.order_management_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/davidcapilla/order_management_kata/OrderManagementKataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.davidcapilla.order_management_kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
