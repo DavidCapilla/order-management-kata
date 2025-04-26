a1ad4c1ab84fa760c406d4c15c277c43669932fb
David Capilla Guilarte <david.capilla.guilarte@ast.cam.ac.uk>
2025-04-26 17:22:56 +0100

    dcg | Repackage

--- src/main/java/io/github/davidcapilla/order_management_kata/order/Order.java
+++ src/main/java/io/github/davidcapilla/order_management_kata/order/Order.java
@@ -2,7 +2,7 @@ package io.github.davidcapilla.order_management_kata.order;
 
 import io.github.davidcapilla.order_management_kata.customer.CustomerDetails;
 import io.github.davidcapilla.order_management_kata.product.Product;
-import io.github.davidcapilla.order_management_kata.payment.gateway.PaymentDetails;
+import io.github.davidcapilla.order_management_kata.payment.PaymentDetails;
 import java.util.List;
 
 public record Order(List<Product> products,
