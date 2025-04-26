6a48c1897271eec425ddff5025692a64980fd978
David Capilla Guilarte <david.capilla.guilarte@ast.cam.ac.uk>
2025-04-26 16:40:47 +0100

    dcg | Creation category model

---
+++ src/main/java/io/github/davidcapilla/order_management_kata/model/Category.java
@@ -0,0 +1,28 @@
+package io.github.davidcapilla.order_management_kata.model;
+
+import java.util.Optional;
+import lombok.Data;
+
+@Data
+public class Category {
+
+    private final Long id;
+    private final String name;
+    private final Category parentCategory;
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.parentCategory = null;
+    }
+
+    public Category(Long id, String name, Category parentCategory) {
+        this.id = id;
+        this.name = name;
+        this.parentCategory = parentCategory;
+    }
+
+    public Optional<Category> getParentCategory() {
+        return Optional.ofNullable(parentCategory);
+    }
+}
